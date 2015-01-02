b705f98f6a59fbc83ef6e3f2587d67c344715180
Jim Veneskey <github@strifecrafter.com>
2015-01-01 19:03:31 -0500

    Initial Commit

---
+++ Funky Unit Converter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Funky Unit Converter
+//
+//  Created by Jim Veneskey on 1/1/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Funky Unit Converter/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Funky Unit Converter
+//
+//  Created by Jim Veneskey on 1/1/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
